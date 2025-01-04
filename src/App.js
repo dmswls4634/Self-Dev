4584d82de8a8e1e8a390b1d06728064e9f180a29
dmswls4634 <dmswls4634@naver.com>
2025-01-04 20:38:01 +0900

    React 31~33

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import './App.css';
-import A from './R029_ShallowEqual'
+import ReturnMap from './R033_ReturnMap'
 
 function App() {
   return (
     <div className="App">
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <A/>
+      <ReturnMap/>
     </div>
   );
 }
