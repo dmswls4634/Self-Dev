07b531c7f834a04bb8bb6f61d0f12a730fd31730
dmswls4634 <dmswls4634@naver.com>
2025-01-09 19:17:50 +0900

    GET/POST

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
-//import './App.css';
-import Sweeralert2Confirm from './R058_Sweetalert2Confirm'
+import AxiosPost from './R062_AxiosPost'
 import 'bootstrap/dist/css/bootstrap.css'
 
 function App() {
@@ -8,7 +7,7 @@ function App() {
     <div className="App">
       <h1>Start React 200!</h1>
       <p>CSS 적용하기</p>
-      <Sweeralert2Confirm/>
+      <AxiosPost/>
     </div>
   );
 }
